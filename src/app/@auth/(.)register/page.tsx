faa21a917629493e8ab67c68d391fc6885616b88
HuyBrox <128505633+HuyBrox@users.noreply.github.com>
2025-09-13 05:12:43 +0700

    commit lúc 5h sáng :))

--- src/app/@auth/(.)register/page.tsx
+++ src/app/@auth/(.)register/page.tsx
@@ -1,5 +1,21 @@
+"use client"
+
 import { AuthModal } from "@/components/auth"
+import { useRouter } from "next/navigation"
+import { useEffect, useState } from "react"
 
 export default function RegisterModal() {
-  return <AuthModal />
+  const router = useRouter()
+  const [isOpen, setIsOpen] = useState(false)
+
+  useEffect(() => {
+    setIsOpen(true)
+  }, [])
+
+  const handleClose = () => {
+    setIsOpen(false)
+    setTimeout(() => router.back(), 150)
+  }
+
+  return <AuthModal isOpen={isOpen} onClose={handleClose} defaultMode="register" />
 }
