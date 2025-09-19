02c0ae3bb89a0c3a13725557ab6bfd1eca16d5b4
Tran Anh Thai <161110835+tranathai@users.noreply.github.com>
2025-09-19 23:14:22 +0700

    add OTP registration flow
    - Integrated /send-otp-register and /register APIs via authApi
    - Updated AuthForm with OTP request
    - Updated store/index.ts to include authApi reducer & middleware

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { userApi } from "./services/userApi";
+import { authApi } from "@/store/services/authApi"
+
 
 export const store = configureStore({
   reducer: {
     [userApi.reducerPath]: userApi.reducer,
+    [authApi.reducerPath]: authApi.reducer, // ✅ thêm reducer của authApi
+
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(userApi.middleware),
+    getDefaultMiddleware().concat(userApi.middleware).concat(authApi.middleware), // ✅ thêm middleware của authApi
 });
 
 export type RootState = ReturnType<typeof store.getState>;
