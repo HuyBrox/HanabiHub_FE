ec418d115609f94f58a7b055992aaab67341c437
Võ Ngọc Trà <vongoctra2@dtu.edu.vn>
2025-11-15 12:51:16 +0700

    commit

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,24 +1,36 @@
+// src/store/index.ts
 // Cấu hình Redux store chính của app
 import { configureStore } from "@reduxjs/toolkit";
+
 import { userApi } from "./services/userApi";
 import { authApi } from "./services/authApi";
 import { messageApi } from "./services/messageApi";
 import { flashcardApi } from "./services/flashcardApi";
 import { courseApi } from "./services/courseApi";
+
+//rứa ren mấy cái ni báo lỗi đc
+/* 👇 THÊM MỚI: Users Admin API */
+import { usersAdminApi } from "./services/admin/usersAdminApi";
+
 import authReducer from "./slices/authSlice";
 import {
-  authMiddleware, // để tự động refresh token
-  authListenerMiddleware, // để lắng nghe các sự kiện auth(login, logout)
+  authMiddleware,              // để tự động refresh token
+  authListenerMiddleware,      // để lắng nghe các sự kiện auth(login, logout)
 } from "./middleware/authMiddleware";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+
     [userApi.reducerPath]: userApi.reducer,
     [authApi.reducerPath]: authApi.reducer,
     [messageApi.reducerPath]: messageApi.reducer,
     [flashcardApi.reducerPath]: flashcardApi.reducer,
     [courseApi.reducerPath]: courseApi.reducer,
+
+
+    /* 👇 THÊM MỚI */
+    [usersAdminApi.reducerPath]: usersAdminApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
@@ -27,6 +39,11 @@ export const store = configureStore({
       .concat(messageApi.middleware)
       .concat(flashcardApi.middleware)
       .concat(courseApi.middleware)
+
+      /* 👇 THÊM MỚI */
+      .concat(usersAdminApi.middleware)
+
+      // auth middlewares giữ nguyên thứ tự như cũ
       .concat(authMiddleware)
       .prepend(authListenerMiddleware.middleware),
 });
